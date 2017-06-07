7307a36d6f21d63fd7ca25b285dd168273635003
Ingvar Stepanyan <me@rreverser.com>
2017-06-08 00:30:02 +0100

    Add few more napi_create_* impls

--- src/values/create.js
+++ src/values/create.js
@@ -10,3 +10,19 @@ export function napi_create_string_utf8(env, str, length, result) {
 export function napi_create_number(env, value, result) {
 	return setValue(result, value);
 }
+
+export function napi_create_object(env, result) {
+	return setValue(result, {});
+}
+
+export function napi_create_array(env, result) {
+	return setValue(result, []);
+}
+
+export function napi_create_array_with_length(env, length, result) {
+	return setValue(result, new Array(length));
+}
+
+export function napi_create_symbol(env, description, result) {
+	return setValue(result, Symbol(UTF8ToString(description)));
+}
