f319a588b04d95879e6f480a444a0b7af99d7902
Ingvar Stepanyan <me@rreverser.com>
2017-06-09 12:07:36 +0100

    Add common.mustCall helper

--- test/common.js
+++ test/common.js
@@ -1,3 +1,38 @@
+const mustCallChecks = [];
+
 module.exports = {
-    buildType: 'debug'
+	buildType: 'debug',
+
+	mustCall(fn = () => {}, expected = 1) {
+		const context = {
+			expected,
+			actual: 0,
+			stack: new Error().stack,
+			name: fn.name || '<anonymous>',
+		};
+
+		mustCallChecks.push(context);
+
+		return function() {
+			context.actual++;
+			return fn.apply(this, arguments);
+		};
+	},
+
+	runCallChecks() {
+		const failed = mustCallChecks.filter(context => {
+			return context.actual !== context.expected;
+		});
+
+		mustCallChecks.length = 0;
+
+		failed.forEach(context => {
+			const message = `Mismatched ${context.name} function calls. Expected ${context.expected}, actual ${context.actual}.`;
+			const e = new Error(message);
+			const stack = context.stack.split('\n').slice(2);
+			stack.unshift(message);
+			e.stack = stack.join('\n');
+			throw e;
+		});
+	},
 };
--- test/run.js
+++ test/run.js
@@ -7,41 +7,45 @@ const { promisify } = require('util');
 const fs = require('fs');
 const readDir = promisify(fs.readdir);
 const lstat = promisify(fs.lstat);
+const common = require('./common');
 
 require('source-map-support').install();
 
 (async () => {
-    let dirs = await readDir('.');
+	let dirs = await readDir('.');
 
-    let tests = [];
+	let tests = [];
 
-    await Promise.all(dirs.map(async dir => {
-        if (dir.startsWith('.')) return;
-        if (!(await lstat(dir)).isDirectory()) return;
+	await Promise.all(
+		dirs.map(async dir => {
+			if (dir.startsWith('.')) return;
+			if (!(await lstat(dir)).isDirectory()) return;
 
-        for (let filename of await readDir(dir)) {
-            if (/^test.*\.js$/.test(filename)) {
-                tests.push(`${dir}/${filename}`);
-            }
-        }
-    }));
+			for (let filename of await readDir(dir)) {
+				if (/^test.*\.js$/.test(filename)) {
+					tests.push(`${dir}/${filename}`);
+				}
+			}
+		})
+	);
 
-    for (let test of tests) {
-        tap.test(test, t => {
-            try {
-                require(`./addons-napi/${test}`);
-            } catch (e) {
-                let err = e;
-                if (typeof err === 'string') {
-                    err = new Error();
-                    err.stack = e.replace(/at[^]*abort.*$/m, '');
-                }
-                throw err;
-            }
-            t.end();
-        });
-    }
+	for (let test of tests) {
+		tap.test(test, t => {
+			try {
+				require(`./addons-napi/${test}`);
+			} catch (e) {
+				let err = e;
+				if (typeof err === 'string') {
+					err = new Error();
+					err.stack = e.replace(/at[^]*abort.*$/m, '');
+				}
+				throw err;
+			}
+			common.runCallChecks();
+			t.end();
+		});
+	}
 })().catch(err => {
-    console.error(err);
-    process.exit(1);
+	console.error(err);
+	process.exit(1);
 });
