6e864bf0977be81ddc5a4917cab49bd046fe2f5d
Ingvar Stepanyan <me@rreverser.com>
2019-02-24 22:26:15 +0000

    Simplify building code

--- test/build.js
+++ test/build.js
@@ -4,70 +4,82 @@ process.chdir(`${__dirname}/js-native-api`);
 
 const { promisify } = require('util');
 const fs = require('fs');
+const mkdir = promisify(fs.mkdir);
 const readDir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
 const lstat = promisify(fs.lstat);
-const exec = promisify(require('child_process').exec);
-const mkdirp = promisify(require('mkdirp'));
+const { spawn } = require('child_process');
 const PQueue = require('p-queue');
 
-const emccParams = [
-	`--js-library ${require.resolve('../index.dist.js')}`,
-	`-I ${__dirname}`,
-	`-O2`,
-	`--memory-init-file 0`,
-	`-g4`,
-	`-s DEMANGLE_SUPPORT=1`,
-	`-s ASSERTIONS=2`,
-	`-s SAFE_HEAP=1`,
-	`-s ALIASING_FUNCTION_POINTERS=0`,
-].join(' ');
+const emcc = require('which').sync('emcc');
 
-let totalCount = 0;
-let finishedCount = 0;
+const emccParams = [
+	'--js-library',
+	require.resolve('../index.dist.js'),
+	'-I',
+	__dirname,
+	'-O2',
+	'--memory-init-file',
+	'0',
+	'-g4',
+	'-s',
+	'DEMANGLE_SUPPORT=1',
+	'-s',
+	'ASSERTIONS=2',
+	'-s',
+	'SAFE_HEAP=1',
+	'-s',
+	'ALIASING_FUNCTION_POINTERS=0',
+];
 
 (async () => {
-	let dirs = await readDir('.');
-	let pqueue = new PQueue({ concurrency: 4 });
+	let pQueue = new PQueue({ concurrency: 4, autoStart: false });
 
-	await Promise.all(
-		dirs.map(async dir => {
-			if (dir.startsWith('.')) return;
-			if (!(await lstat(dir)).isDirectory()) return;
+	for (let dir of await readDir('.')) {
+		if (dir.startsWith('.')) continue;
+		if (!(await lstat(dir)).isDirectory()) continue;
 
-			let gypText = await readFile(`${dir}/binding.gyp`, 'utf-8');
+		let gypText = await readFile(`${dir}/binding.gyp`, 'utf-8');
 
-			// not really JSON, but most of them can be parsed with one little trick...
-			gypText = gypText.replace(/'/g, '"');
+		// not really JSON, but most of them can be parsed with one little trick...
+		gypText = gypText.replace(/'/g, '"');
 
-			let gyp = JSON.parse(gypText);
+		let gyp = JSON.parse(gypText);
 
-			let bindingDir = `${dir}/build/debug`;
+		let bindingDir = `${dir}/build/debug`;
 
-			await mkdirp(bindingDir);
+		await mkdir(bindingDir, { recursive: true });
 
-			await Promise.all(
-				gyp.targets.map(async target => {
-					let sources = target.sources.map(name => `${dir}/${name}`);
-					let dest = `${bindingDir}/${target.target_name}.js`;
+		for (let target of gyp.targets) {
+			let myIndex = pQueue.size;
 
-					let myIndex = totalCount++;
-					let shortCmd = `emcc ${sources.join(' ')} -o ${dest}`;
-					let cmd = `${shortCmd} ${emccParams}`;
-					console.log(`[#${myIndex}] Running: ${shortCmd}`);
-					pqueue.add(async () => {
-						let { stderr } = await exec(cmd);
-						console.log(
-							`[#${myIndex}] [${++finishedCount}/${totalCount}] Done: ${shortCmd}`
-						);
-						console.warn(stderr);
+			pQueue.add(async () => {
+				let sources = target.sources.map(name => `${dir}/${name}`);
+				let dest = `${bindingDir}/${target.target_name}.js`;
+
+				let shortCmd = `emcc ${sources.join(' ')} -o ${dest}`;
+				console.log(`[#${myIndex}] Running: ${shortCmd}`);
+				let status = await new Promise((resolve, reject) => {
+					let process = spawn(emcc, [...emccParams, ...sources, '-o', dest], {
+						stdio: ['ignore', 'inherit', 'inherit'],
 					});
-				})
-			);
-		})
-	);
+					process.once('error', reject);
+					process.once('exit', code =>
+						code === 0 ? resolve() : reject(new Error(`Exit code ${code}`))
+					);
+				}).then(() => 'OK', err => err.stack);
+				console.log(
+					`[#${myIndex}] [${pQueue.pending}/${
+						pQueue.size
+					}] Done: ${shortCmd} (${status})`
+				);
+			});
+		}
+	}
 
-	await pqueue.onEmpty;
+	pQueue.start();
+
+	await pQueue.onEmpty;
 })().catch(err => {
 	console.error(err);
 	process.exit(1);
